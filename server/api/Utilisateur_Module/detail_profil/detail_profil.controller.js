9175f1dd2480f18b8b7af375fd956300860814ee
sadioraymond <sadiowalyraymond@gmail.com>
2017-02-26 23:48:51 +0000

    Merge of develop

--- server/api/Utilisateur_Module/detail_profil/detail_profil.controller.js
+++ server/api/Utilisateur_Module/detail_profil/detail_profil.controller.js
@@ -14,104 +14,104 @@ import jsonpatch from 'fast-json-patch';
 import DetailProfil from './detail_profil.model';
 
 function respondWithResult(res, statusCode) {
-  statusCode = statusCode || 200;
-  return function(entity) {
-    if(entity) {
-      return res.status(statusCode).json(entity);
-    }
-    return null;
-  };
+    statusCode = statusCode || 200;
+    return function(entity) {
+        if (entity) {
+            return res.status(statusCode).json(entity);
+        }
+        return null;
+    };
 }
 
 function patchUpdates(patches) {
-  return function(entity) {
-    try {
-      jsonpatch.apply(entity, patches, /*validate*/ true);
-    } catch(err) {
-      return Promise.reject(err);
-    }
+    return function(entity) {
+        try {
+            jsonpatch.apply(entity, patches, /*validate*/ true);
+        } catch (err) {
+            return Promise.reject(err);
+        }
 
-    return entity.save();
-  };
+        return entity.save();
+    };
 }
 
 function removeEntity(res) {
-  return function(entity) {
-    if(entity) {
-      return entity.remove()
-        .then(() => {
-          res.status(204).end();
-        });
-    }
-  };
+    return function(entity) {
+        if (entity) {
+            return entity.remove()
+                .then(() => {
+                    res.status(204).end();
+                });
+        }
+    };
 }
 
 function handleEntityNotFound(res) {
-  return function(entity) {
-    if(!entity) {
-      res.status(404).end();
-      return null;
-    }
-    return entity;
-  };
+    return function(entity) {
+        if (!entity) {
+            res.status(404).end();
+            return null;
+        }
+        return entity;
+    };
 }
 
 function handleError(res, statusCode) {
-  statusCode = statusCode || 500;
-  return function(err) {
-    res.status(statusCode).send(err);
-  };
+    statusCode = statusCode || 500;
+    return function(err) {
+        res.status(statusCode).send(err);
+    };
 }
 
 // Gets a list of DetailProfils
 export function index(req, res) {
-  return DetailProfil.find().populate('user').populate('cours').exec()
-    .then(respondWithResult(res))
-    .catch(handleError(res));
+    return DetailProfil.find().populate('user').populate('profil').exec()
+        .then(respondWithResult(res))
+        .catch(handleError(res));
 }
 
 // Gets a single DetailProfil from the DB
 export function show(req, res) {
-  return DetailProfil.findById(req.params.id).exec()
-    .then(handleEntityNotFound(res))
-    .then(respondWithResult(res))
-    .catch(handleError(res));
+    return DetailProfil.findById(req.params.id).exec()
+        .then(handleEntityNotFound(res))
+        .then(respondWithResult(res))
+        .catch(handleError(res));
 }
 
 // Creates a new DetailProfil in the DB
 export function create(req, res) {
-  return DetailProfil.create(req.body)
-    .then(respondWithResult(res, 201))
-    .catch(handleError(res));
+    return DetailProfil.create(req.body)
+        .then(respondWithResult(res, 201))
+        .catch(handleError(res));
 }
 
 // Upserts the given DetailProfil in the DB at the specified ID
 export function upsert(req, res) {
-  if(req.body._id) {
-    delete req.body._id;
-  }
-  return DetailProfil.findOneAndUpdate({_id: req.params.id}, req.body, {new: true, upsert: true, setDefaultsOnInsert: true, runValidators: true}).exec()
+    if (req.body._id) {
+        delete req.body._id;
+    }
+    return DetailProfil.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, upsert: true, setDefaultsOnInsert: true, runValidators: true }).exec()
 
     .then(respondWithResult(res))
-    .catch(handleError(res));
+        .catch(handleError(res));
 }
 
 // Updates an existing DetailProfil in the DB
 export function patch(req, res) {
-  if(req.body._id) {
-    delete req.body._id;
-  }
-  return DetailProfil.findById(req.params.id).exec()
-    .then(handleEntityNotFound(res))
-    .then(patchUpdates(req.body))
-    .then(respondWithResult(res))
-    .catch(handleError(res));
+    if (req.body._id) {
+        delete req.body._id;
+    }
+    return DetailProfil.findById(req.params.id).exec()
+        .then(handleEntityNotFound(res))
+        .then(patchUpdates(req.body))
+        .then(respondWithResult(res))
+        .catch(handleError(res));
 }
 
 // Deletes a DetailProfil from the DB
 export function destroy(req, res) {
-  return DetailProfil.findById(req.params.id).exec()
-    .then(handleEntityNotFound(res))
-    .then(removeEntity(res))
-    .catch(handleError(res));
+    return DetailProfil.findById(req.params.id).exec()
+        .then(handleEntityNotFound(res))
+        .then(removeEntity(res))
+        .catch(handleError(res));
 }
