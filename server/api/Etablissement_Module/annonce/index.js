66308ffa9da4b715361c1c2109f47fadf1abde04
sadioraymond <sadiowalyraymond@gmail.com>
2017-03-13 15:08:17 +0000

    Modification du seed des annonces

--- server/api/Etablissement_Module/annonce/index.js
+++ server/api/Etablissement_Module/annonce/index.js
@@ -6,6 +6,7 @@ var controller = require('./annonce.controller');
 var router = express.Router();
 
 router.get('/', controller.index);
+router.get('/etablissement/:id', controller.getAnnoceByEtablissement);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
 router.put('/:id', controller.upsert);
