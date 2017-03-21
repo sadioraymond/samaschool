c9d1f4f609070ce1159110e80bc23e303e395251
sadioraymond <sadiowalyraymond@gmail.com>
2017-03-21 11:39:16 +0000

    Ajout des fonctions isProf, isEtudiant et isUsersimple dans le provider userProvider

--- client/app/factory/userProvider/userProvider.service.js
+++ client/app/factory/userProvider/userProvider.service.js
@@ -3,31 +3,75 @@ const angular = require('angular');
 
 /*@ngInject*/
 export function userProviderService($http, $q) {
-  this.msg = false;
-  this.varbi = null;
-  this.partage = function (test) {
-    test.dismiss('cancel');
-  }
+    this.msg = false;
+    this.varbi = null;
+    this.partage = function(test) {
+        test.dismiss('cancel');
+    }
 
-  // AngularJS will instantiate a singleton by calling "new" on this function
-  this.ListUser = function () {
-    var deferred = $q.defer();
-    var liste = [];
-    $http.get('/api/users', {
-      cache: true
-    }).then(function (list) {
-      liste = list.data;
-      deferred.resolve(liste);
+    // AngularJS will instantiate a singleton by calling "new" on this function
+    this.ListUser = function() {
+        var deferred = $q.defer();
+        var liste = [];
+        $http.get('/api/users', {
+            cache: true
+        }).then(function(list) {
+            liste = list.data;
+            deferred.resolve(liste);
 
-    });
-    liste = deferred.promise;
+        });
+        liste = deferred.promise;
 
-    return liste;
+        return liste;
 
-  }
+    }
+
+    this.isProf = function(id) {
+        var deferred = $q.defer();
+        var liste = [];
+        $http.get('/api/profils/isprof/' + id, {
+            cache: true
+        }).then(function(list) {
+            liste = list.data;
+            deferred.resolve(liste);
+
+        });
+        liste = deferred.promise;
+        return liste;
+    }
+
+
+    this.isEtudiant = function(id) {
+        var deferred = $q.defer();
+        var liste = [];
+        $http.get('/api/profils/isetudiant/' + id, {
+            cache: true
+        }).then(function(list) {
+            liste = list.data;
+            deferred.resolve(liste);
+
+        });
+        liste = deferred.promise;
+        return liste;
+    }
+
+
+    this.isUsersimple = function(id) {
+        var deferred = $q.defer();
+        var liste = [];
+        $http.get('/api/profils/isusersimple/' + id, {
+            cache: true
+        }).then(function(list) {
+            liste = list.data;
+            deferred.resolve(liste);
+
+        });
+        liste = deferred.promise;
+        return liste;
+    }
 
 }
 
 export default angular.module('samaschoolApp.userProvider', [])
-  .service('userProvider', userProviderService)
-  .name;
+    .service('userProvider', userProviderService)
+    .name;
