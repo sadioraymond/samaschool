f0bf6f237ad1d7b657121237e6a4efb521295e3b
Ahmadou Waly Ndiaye <ahmadouwalyndiaye@gmail.com>
2017-03-26 02:12:28 +0000

    **Mise en place de Modal au niveau du boutton publier **Integration de getClasseByUser dans createcourse.component

--- client/app/factory/suivi_cours_classeProvider/suivi_cours_classeProvider.service.js
+++ client/app/factory/suivi_cours_classeProvider/suivi_cours_classeProvider.service.js
@@ -3,25 +3,24 @@ const angular = require('angular');
 
 /*@ngInject*/
 export function suiviCoursClasseProviderService($http, $q) {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  // AngularJS will instantiate a singleton by calling "new" on this function
+  this.getCoursByClasse = function (classe) {
+    var deferred = $q.defer();
+    var liste = [];
+    $http.get('/api/suivi_cours_classes/classe/' + classe, {
+      cache: true
+    }).then(function (list) {
+      liste = list.data;
+      deferred.resolve(liste);
 
-    this.getCoursByClasse = function(classe) {
-        var deferred = $q.defer();
-        var liste = [];
-        $http.get('/api/suivi_cours_classes/classe/' + classe, {
-            cache: true
-        }).then(function(list) {
-            liste = list.data;
-            deferred.resolve(liste);
+    });
+    liste = deferred.promise;
 
-        });
-        liste = deferred.promise;
+    return liste;
 
-        return liste;
-
-    }
+  }
 }
 
 export default angular.module('samaschoolApp.suivi_cours_classeProvider', [])
-    .service('suiviCoursClasseProvider', suiviCoursClasseProviderService)
-    .name;
+  .service('suiviCoursClasseProvider', suiviCoursClasseProviderService)
+  .name;
