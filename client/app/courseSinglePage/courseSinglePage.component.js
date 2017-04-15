fa9efb473ec0b234024fe5264f2d5444a6565bb5
Ahmadou Waly Ndiaye <ahmadouwalyndiaye@gmail.com>
2017-04-15 16:06:37 +0000

    Mise en page du cours en fonction de leur chapitre

--- client/app/courseSinglePage/courseSinglePage.component.js
+++ client/app/courseSinglePage/courseSinglePage.component.js
@@ -12,12 +12,13 @@ export class CourseSinglePageComponent {
   souscategorieProvider;
   chapitreProvider;
   userProvider;
+  LesChapitres = [];
   /*@ngInject*/
   constructor(jsFonctions, coursProvider, $stateParams, souscategorieProvider, chapitreProvider, userProvider) {
     // setTimeout(() => {
     this.$stateParams = $stateParams;
     this.userProvider = userProvider;
-    console.log('param =>', this.$stateParams)
+    console.log('params =>', this.$stateParams);
     // }, 50);
     this.message = 'Hello';
     this.jsFonctions = jsFonctions;
@@ -37,22 +38,41 @@ export class CourseSinglePageComponent {
       this.souscat = list;
       console.log('La Sous Catégorie', this.souscat);
     });
+    // Recuperation du cours en passant l'url
     this.coursProvider.FindById(this.$stateParams.idCours).then(list => {
       this.LeCours = list;
       console.log('objet cours =>>', this.LeCours);
-        this.userProvider.findById(this.LeCours.user).then(list => {
-          this.Leprof = list;
-          console.log('Le prof qui a cree le cours =>>', this.Leprof);
+      // si le cours a des chapitres
+      if (!this.LeCours.contenu) {
+        // Recuperation des chapitres en passant l'url
+        this.chapitreProvider.getChapitreByCours(this.$stateParams.idCours).then(list => {
+          this.LesChapitres = list;
+            console.info('les chapitre du cours =>>', this.LesChapitres, 'et nombre ', this.LesChapitres.length);
+            // si un chapitre est choisi
+            // recherche du chapitre choisi
+            if(this.$stateParams.idChap){
+              this.LesChapitres.map((chapitre) => {
+                if(chapitre._id == this.$stateParams.idChap){
+                  this.LeChapitre = chapitre;
+                  console.log('le chapitre ===>>> ', this.LeChapitre);
+
+                  // recuperation du contenu du chapitre
+                  this.chapitreProvider.getFichierByChapitre(chapitre._id).then(list => {
+                    this.LeContenuDuChapitre = list[0];
+                    console.info('le contenu du chapitre du cours =>>', this.LeContenuDuChapitre);
+                    this.LeChapitre.contenuChap = this.LeContenuDuChapitre;
+                  });
+                }
+              });
+            }
         });
-    });
-    this.chapitreProvider.getChapitreByCours(this.$stateParams.idCours).then(list => {
-      this.LesChapitres = list;
-      if (this.LesChapitres.length == 0) {
-        console.log('Liste Vide chap', this.LesChapitres);
-      } else {
-        console.info('les chapitre du cours =>>', this.LesChapitres, 'et nombre ', this.LesChapitres.length);
       }
+      this.userProvider.findById(this.LeCours.user).then(list => {
+        this.Leprof = list;
+        console.log('Le prof qui a cree le cours =>>', this.Leprof);
+      });
     });
+
   }
 }
 
