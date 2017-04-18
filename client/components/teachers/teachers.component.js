a984865dff6d0fb7f50b109ee5f94cc68f75c3fd
pach93 <dergui2006@hotmail.com>
2017-04-18 17:49:30 +0000

    integration des donnees top professeurs

--- client/components/teachers/teachers.component.js
+++ client/components/teachers/teachers.component.js
@@ -3,15 +3,28 @@ const angular = require('angular');
 
 export class teachersComponent {
   /*@ngInject*/
-  constructor() {
-    this.message = 'World';
+// Les services à injecter
+profilProvider;
+// Les varibles à utiliser
+  LesProfsPlusSuivis;
+  constructor(profilProvider) {
+    this.profilProvider = profilProvider;
+  }
+
+  $onInit(){
+    this.profilProvider.ProfesseurPlusSuivi().then(list => {
+      this.LesProfsPlusSuivis = list;
+      console.log("les profs plus suivis", list);
+    })
   }
 }
 
+teachersComponent.$inject = ["profilProvider", "jsFonctions"];
 export default angular.module('samaschoolApp.teachers', [])
   .component('teachers', {
     template: require('./teachers.html'),
     bindings: { message: '<' },
-    controller: teachersComponent
+    controller: teachersComponent,
+    controllerAs: 'vm'
   })
   .name;
