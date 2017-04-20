a9db6fcc66efa13a9bc6e1fba76faf834ef5fff9
pach93 <dergui2006@hotmail.com>
2017-04-20 04:07:03 +0000

    dynamisation des annonces dans la page etablissement

--- client/app/app.js
+++ client/app/app.js
@@ -114,17 +114,17 @@ angular.module('samaschoolApp', [ngCookies, ngResource, ngSanitize, 'btford.sock
             nav: true,
             dots: false,
             autoplay: true,
-            responsive: {
-              0: {
-                items: 1
-              },
-              600: {
-                items: 2
-              },
-              1000: {
-                items: 3
-              }
-            }
+            // responsive: {
+            //   0: {
+            //     items: 1
+            //   },
+            //   600: {
+            //     items: 2
+            //   },
+            //   1000: {
+            //     items: 3
+            //   }
+            // }
           };
           // }
           var customOptions = scope.$eval($(element).attr('data-options'));
