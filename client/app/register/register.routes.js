0f69e6462af973814f6520e45534d9d17b1417c8
Ahmadou Waly Ndiaye <ahmadouwalyndiaye@gmail.com>
2017-04-28 14:48:36 +0000

    **Lien inscription etablissement au niveau du footer **changement de la route /register par /register/etablissement

--- client/app/register/register.routes.js
+++ client/app/register/register.routes.js
@@ -4,7 +4,7 @@ export default function($stateProvider) {
   'ngInject';
   $stateProvider
     .state('register', {
-      url: '/register',
+      url: '/register/etablissement',
       template: '<register></register>'
     });
 }
