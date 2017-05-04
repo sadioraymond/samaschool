a7dd78034e2575199e11d1b450e80aa5ce30fe32
Ahmadou Waly Ndiaye <ahmadouwalyndiaye@gmail.com>
2017-05-04 14:11:34 +0000

    modification photo couverture etablissement avec reload

--- client/app/factory/ouvreDialogProvider/ouvreDialogProvider.service.js
+++ client/app/factory/ouvreDialogProvider/ouvreDialogProvider.service.js
@@ -3,30 +3,38 @@ const angular = require('angular');
 
 /*@ngInject*/
 export function ouvreDialogProviderService() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-    this.uploadFile = function(e, imgTag) {
-        let fichier = e.target.files[0]
-        if (fichier.type.indexOf('image') > -1) {
-            var lecteur = new FileReader();
-            lecteur.onload = function(e) {
-                console.log('log', e);
-                let img = document.querySelector(`#${imgTag}`);
-                img.setAttribute('src', e.target.result);
-                img.setAttribute('ng-src', '');
-                console.log('imgtag', img);
-                console.log('ready', lecteur.readyState);
-            }
-            lecteur.readAsDataURL(fichier);
-            console.log('bandi bi', fichier);
-            console.log('readystate', lecteur.readyState);
-            return fichier;
+  // AngularJS will instantiate a singleton by calling "new" on this function
+  this.uploadFile = function (e, imgTag, style, url) {
+      console.log('url =>', url)
+    let fichier = e.target.files[0]
+    if (fichier.type.indexOf('image') > -1) {
+      var lecteur = new FileReader();
+      lecteur.onload = function (e) {
+        console.log('log', e);
+        let img = document.querySelector(`#${imgTag}`);
+        if (!style) {
+          img.setAttribute('src', e.target.result);
+          img.setAttribute('ng-src', '');
         } else {
-            alert('Ce n\'est pas une image');
+          let objStyle = `{'background-image': 'url(assets/images/upload/${url}/${e.target.result})'}`
+          img.setAttribute('style', `background-image:url('assets/upload/Etablissement/${e.target.result}'); height: 500px; border-bottom: 4px solid #fff; background-size: cover`);
+        //   img.setAttribute('style', '');
+          //   img.setAttribute('ng-src', '');
         }
-        // console.log('target', e.target.files);
+        console.log('imgtag', img);
+        console.log('ready', lecteur.readyState);
+      }
+      lecteur.readAsDataURL(fichier);
+      console.log('bandi bi', fichier);
+      console.log('readystate', lecteur.readyState);
+      return fichier;
+    } else {
+      alert('Ce n\'est pas une image');
     }
+    // console.log('target', e.target.files);
+  }
 }
 
 export default angular.module('samaschoolApp.ouvreDialogProvider', [])
-    .service('ouvreDialogProvider', ouvreDialogProviderService)
-    .name;
+  .service('ouvreDialogProvider', ouvreDialogProviderService)
+  .name;
