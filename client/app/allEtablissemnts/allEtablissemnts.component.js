74e70291adb0b80a93055bc88cc6a53ca64253f3
Ahmadou Waly Ndiaye <ahmadouwalyndiaye@gmail.com>
2017-05-09 14:22:36 +0000

    mise en place du template et de la fonctionnalité allEtablissement

--- client/app/allEtablissemnts/allEtablissemnts.component.js
+++ client/app/allEtablissemnts/allEtablissemnts.component.js
@@ -7,16 +7,31 @@ import routes from './allEtablissemnts.routes';
 
 export class AllEtablissemntsComponent {
   /*@ngInject*/
-  constructor() {
-    this.message = 'Hello';
+  constructor(jsFonctions, etablissementProvider) {
+    this.jsFonctions = jsFonctions
+    this.etablissementProvider = etablissementProvider
+  }
+  $onInit() {
+    angular.element(document)
+      .ready(() => {
+        setTimeout(() => {
+          this.jsFonctions.pluginScript();
+          this.jsFonctions.otherScript();
+        }, 0);
+      });
+    this.etablissementProvider.listeEtablissement().then(list => {
+      this.LesEtablissements = list;
+      console.log('les etablissements', list);
+    })
   }
 }
 
+AllEtablissemntsComponent.$inject = ["jsFonctions", "etablissementProvider"]
 export default angular.module('samaschoolApp.allEtablissemnts', [uiRouter])
   .config(routes)
   .component('allEtablissemnts', {
     template: require('./allEtablissemnts.html'),
     controller: AllEtablissemntsComponent,
-    controllerAs: 'allEtablissemntsCtrl'
+    controllerAs: 'vm'
   })
   .name;
